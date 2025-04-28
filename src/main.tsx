f38298f9a24548c1f74782a14f0cfdb663d1e8f9
Leonardo <leonardo.abreu.de.paulo@gmail.com>
2025-04-28 17:57:58 +0100

    Add React Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ClerkProvider } from "@clerk/clerk-react";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 import "./index.css";
 import App from "./App.tsx";
-
+import Profile from "./Profile.tsx";
 // CLERK
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 if (!PUBLISHABLE_KEY) {
@@ -18,12 +19,17 @@ const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-      <ConvexProvider client={convex}>
-        <Layout>
-          <App />
-        </Layout>
-      </ConvexProvider>
-    </ClerkProvider>
+    <BrowserRouter>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+        <ConvexProvider client={convex}>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<App />} />
+              <Route path="/profile" element={<Profile />} />
+            </Routes>
+          </Layout>
+        </ConvexProvider>
+      </ClerkProvider>
+    </BrowserRouter>
   </StrictMode>
 );
