4472f9b1f7e559f218284156f3dbd215e111c36c
Leonardo <leonardo.abreu.de.paulo@gmail.com>
2025-04-28 18:06:21 +0100

    Add Clerk with Convex

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { ClerkProvider } from "@clerk/clerk-react";
+import { ClerkProvider, useAuth } from "@clerk/clerk-react";
 import { BrowserRouter, Route, Routes } from "react-router";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
 
 import "./index.css";
 import App from "./App.tsx";
@@ -13,7 +15,6 @@ if (!PUBLISHABLE_KEY) {
 }
 
 // CONVEX
-import { ConvexProvider, ConvexReactClient } from "convex/react";
 import Layout from "./Layout.tsx";
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
 
@@ -21,14 +22,14 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-        <ConvexProvider client={convex}>
+        <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
           <Layout>
             <Routes>
               <Route path="/" element={<App />} />
               <Route path="/profile" element={<Profile />} />
             </Routes>
           </Layout>
-        </ConvexProvider>
+        </ConvexProviderWithClerk>
       </ClerkProvider>
     </BrowserRouter>
   </StrictMode>
